e085b7533931b8f301679efc1657e52f4b31f20f
Paul Stefan Ort <mail@paulstefanort.com>
2013-02-25 19:26:34 -0600

    Add NSString+Parlance category.

---
+++ Objective-C/Parlance/Parlance/NSString+Parlance.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Parlance.h
+//  Parlance
+//
+//  Created by Paul Stefan Ort on 2/25/13.
+//  Copyright (c) 2013 Paul Stefan Ort. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Parlance)
+
+@end
---
+++ Objective-C/Parlance/Parlance/NSString+Parlance.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Parlance.m
+//  Parlance
+//
+//  Created by Paul Stefan Ort on 2/25/13.
+//  Copyright (c) 2013 Paul Stefan Ort. All rights reserved.
+//
+
+#import "NSString+Parlance.h"
+
+@implementation NSString (Parlance)
+
+@end
