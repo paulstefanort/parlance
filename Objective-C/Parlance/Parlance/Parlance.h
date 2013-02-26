74435fbadc8d496384945b7764f7250befe84e7a
Paul Stefan Ort <mail@paulstefanort.com>
2013-02-25 20:00:32 -0600

    Process word structure.

--- Objective-C/Parlance/Parlance/Parlance.h
+++ Objective-C/Parlance/Parlance/Parlance.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NSString+Parlance.h"
 
 @interface Parlance : NSObject
 
--- Objective-C/Parlance/Parlance/Parlance.m
+++ Objective-C/Parlance/Parlance/Parlance.m
@@ -52,4 +52,43 @@
     
 }
 
+- (NSString *)structureForWord:(NSString *)word {
+    NSString *wordStructure = word;
+    // process vowel clusters
+    for (NSString *vowelCluster in vowelClusters) {
+        wordStructure = [wordStructure stringByReplacingOccurrencesOfString:vowelCluster withString:[NSString stringWithFormat:@"+%@+", vowelCluster]];
+    }
+    // process consonant clusters
+    for (NSString *consonantCluster in consonantClusters) {
+        wordStructure = [wordStructure stringByReplacingOccurrencesOfString:consonantCluster withString:[NSString stringWithFormat:@"+%@+", consonantCluster]];
+    }
+    wordStructure = [wordStructure cleanup];
+    
+    // loop through fragments
+    NSMutableArray *wordFragments = [wordStructure componentsSeparatedByString:@"+"];
+    for (int i = 0; i < wordFragments.count; i++) {
+        NSString *wordFragment = [wordFragments objectAtIndex:i];
+        // skip if fragment matches vowel cluster
+        if ([vowelClusters containsObject:wordFragment]) {
+            break;
+        }
+        // skip if fragment matches consonant cluster
+        if ([consonantClusters containsObject:wordFragment]) {
+            break;
+        }
+        // otherwise divide fragment into vowels
+        for (NSString *vowel in vowels) {
+            wordFragment = [wordFragment stringByReplacingOccurrencesOfString:vowel withString:[NSString stringWithFormat:@"+%@+", vowel]];
+        }
+        // and consonants
+        for (NSString *consonant in consonants) {
+            wordFragment = [wordFragment stringByReplacingOccurrencesOfString:consonant withString:[NSString stringWithFormat:@"+%@+", consonant]];
+        }
+        [wordFragments replaceObjectAtIndex:i withObject:wordFragment];
+    }
+    wordStructure = [wordFragments componentsJoinedByString:@"+"];
+    wordStructure = [wordStructure cleanup];
+    return wordStructure;
+}
+
 @end
