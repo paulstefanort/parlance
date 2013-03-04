f7ee62852fb831a2d34e7be32d8c67fa71b4c02c
Paul Stefan Ort <mail@paulstefanort.com>
2013-03-03 22:53:08 -0600

    Continue UI of Mac app.

--- Objective-C/Parlance/Parlance/AppDelegate.h
+++ Objective-C/Parlance/Parlance/AppDelegate.h
@@ -11,5 +11,12 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *vowelsTextField;
+@property (weak) IBOutlet NSTextField *vowelClustersTextField;
+@property (weak) IBOutlet NSTextField *consonantsTextField;
+@property (weak) IBOutlet NSTextField *consonantClustersTextField;
+@property (weak) IBOutlet NSTextField *punctuationMarksTextField;
+@property (weak) IBOutlet NSTextField *allowedWordsTextField;
+@property (weak) IBOutlet NSTextField *textTextField;
 
 @end
--- Objective-C/Parlance/Parlance/AppDelegate.m
+++ Objective-C/Parlance/Parlance/AppDelegate.m
@@ -7,12 +7,36 @@
 //
 
 #import "AppDelegate.h"
+#import "Parlance.h"
+
+@interface AppDelegate ()
+@property (strong) Parlance *parlance;
+@end
 
 @implementation AppDelegate
+
+@synthesize parlance;
+@synthesize vowelsTextField, vowelClustersTextField, consonantsTextField, consonantClustersTextField, punctuationMarksTextField, allowedWordsTextField, textTextField;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    parlance = [Parlance new];
+}
+
+- (IBAction)processButtonPressed:(id)sender {
+    NSLog(@"processedButtonPressed");
+    
+    NSString *vowelsString = [vowelsTextField stringValue];
+    NSMutableArray *vowels = [NSMutableArray arrayWithArray:[vowelsString componentsSeparatedByString:@","]];
+    for (int i = 0; i < vowels.count; i++) {
+        NSMutableString *vowel = [NSMutableString stringWithString:[vowels objectAtIndex:i]];
+        vowel = [NSMutableString stringWithString:[vowel stringByReplacingOccurrencesOfString:@" " withString:@""]];
+        [vowels setObject:vowel atIndexedSubscript:i];
+    }
+    [parlance setVowels:vowels];
+    
+    NSLog(@"/processedButtonPressed");
 }
 
 @end
